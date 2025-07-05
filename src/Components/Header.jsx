f1882c14f300c5043280543e35224c56e91a64c9
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-05 12:30:11 +0530

    Added header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-center w-[100dvw] font-bold text-[2em] text-[navy]'>
-      <p>PINNACLE GROUP</p>
+    <div className="flex items-center w-full h-40 font-bold text-[2em] text-blue-700 bg-gradient-to-b from-[#6ffdff] to-[#e6f4f1]">{/* to-[#00c2cb] */}
+        <img src="/logo.webp" alt="logo" className='h-20 !ml-20'/>
     </div>
+
+
   )
 }
 
