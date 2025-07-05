029299bd8264464b2fb71cc599611b158cdd409d
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-05 15:42:03 +0530

    Added property connection

---
+++ src/Components/SocialMedia.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const SocialMedia = () => {
+  return (
+    <div className='flex gap-4 justify-evenly'>
+        <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpinnacleinfra.co.in&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="auto"
+        height="500"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        allowFullScreen={true}
+        className='rounded border-2 border-black'
+                 
+        ></iframe>
+      
+        <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpinnacleinfra.co.in&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="auto"
+        height="500"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        allowFullScreen={true}
+        className='rounded border-2 border-black'
+         
+        ></iframe>
+    </div>
+  )
+}
+
+export default SocialMedia
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,37 @@
+const Home = () => {
+  return (
+    <div className="w-full !p-5">
+      <div className="!py-2 w-full rounded-xl grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
+        {Array.from({ length: 8 }, (_, i) => {
+          const bgColors = [
+            "bg-red-300",
+            "bg-blue-300",
+            "bg-green-300",
+            "bg-yellow-300",
+            "bg-purple-300",
+            "bg-pink-300",
+            "bg-orange-300",
+            "bg-teal-300",
+          ];
+          return (
+            <div
+              key={i}
+              className={`${bgColors[i % bgColors.length]} rounded-lg h-60 flex items-center justify-center text-xl font-semibold shadow-md hover:scale-105 transition-transform duration-300`}
+            >
+              Property {i + 1}
+            </div>
+          );
+        })}
+      </div>
+
+      {/* View More Button */}
+      <div className="w-full mt-6 flex justify-center">
+        <button className="!px-6 !py-2 !mt-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300">
+          View More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
