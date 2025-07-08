a81550156522189c9e1dd9c1dcd7da12d9d900b6
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-08 18:53:02 +0530

    Created Ad Vidoe

--- src/Components/Ad.jsx
+++ src/Components/Ad.jsx
@@ -2,42 +2,17 @@ import React, { useEffect, useState } from "react";
 import Video from "../assets/AdVideo.mp4";
 
 const Ad = () => {
-  const [showAd, setShowAd] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowAd(true);
-    }, 5000); // Show after 5 seconds
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!showAd) return null;
-
   return (
-    <div className="!px-5 ">
-      {/* Fullscreen blur overlay
-      <div className="fixed inset-0 z-40 backdrop-blur-sm bg-black/30"></div>
-
-      <div className="fixed left-5 top-3 z-50 bg-blue-200 !p-4 gap-4 flex flex-col justify-between shadow-lg rounded-xl">
-        <button
-          onClick={() => setShowAd(false)}
-          className="self-end flex items-center font-bold text-red-600 hover:text-red-800 text-xl"
-          aria-label="Close"
-        >
-          <FaTimes className="!mr-1" /> CLOSE AD
-        </button> */}
-
-      {/* Videos */}
+    <div className="!px-5 !mb-16">
       <video
         src={Video}
-        className="relative w-full h-auto max-h-[40em] object-fit rounded-xl"
+        className="relative w-full h-auto max-h-[40em] object-cover rounded-xl"
         muted
         autoPlay
         loop
+        preload="metadata"
+        // poster="/path/to/preview.jpg"
       />
-
-      {/* </div> */}
     </div>
   );
 };
