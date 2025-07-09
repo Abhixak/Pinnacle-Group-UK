d74a329e594d38566b9f2dba3a894ac632916da7
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-09 14:01:29 +0530

    Social Media component changes

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -3,42 +3,40 @@ import React from "react";
 const SocialMedia = () => {
   return (
     <div className="bg-gray-100 !p-5 rounded-xl">
-      <h2 className="text-3xl font-bold !my-5 text-center">
+      <h2 className="text-3xl font-bold !mt-5 !mb-10 text-center">
         Our{" "}
         <span className="text-red-600 underline underline-offset-4">
           Social Profiles
         </span>
       </h2>
+
       <div className="flex flex-col gap-8 !mb-10 justify-center items-center">
-        <div className="w-full flex flex-col items-center">
+        <div className="w-full max-w-[360px] flex flex-col items-center">
           <h2 className="!px-4 !py-2 bg-blue-500 text-white rounded-xl text-3xl font-bold text-center !mb-4">
             Facebook
           </h2>
-          <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpinnacleinfra.co.in&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-            width="340"
-            height="500"
-            style={{ border: "none", overflow: "hidden" }}
-            scrolling="no"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-            allowFullScreen={true}
-            className="rounded-xl border-2 border-black"
-          ></iframe>
-        </div>
 
-        {/* You can uncomment this if you use SnapWidget */}
-        {/* <iframe
-          src="https://snapwidget.com/embed/104774"
-          width="340"
-          height="500"
-          style={{ border: "none", overflow: "hidden" }}
-          scrolling="no"
-          frameBorder="0"
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          allowFullScreen={true}
-          className="rounded border-2 border-black"
-        ></iframe> */}
+          {/* Wrapper to enforce border radius */}
+          <div className="w-full rounded-xl overflow-hidden border-2 border-black">
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpinnacleinfra.co.in&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+              width="100%"
+              height="500"
+              className="rounded-xl"
+              style={{ border: "none" }}
+              scrolling="no"
+              frameBorder="0"
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+              allowFullScreen={true}
+              title="Facebook Page Plugin"
+            ></iframe>
+          </div>
+
+          <p className="text-center text-sm text-gray-500 !mt-4">
+            If the Facebook feed doesn't load, please check your browser settings or try again later.
+            To Visit our Facebook page <a href="https://www.facebook.com/pinnacleinfra.co.in?ref=embed_page" className="underline text-blue-500" target="_blank">Click Here</a> 
+          </p>
+        </div>
       </div>
     </div>
   );
