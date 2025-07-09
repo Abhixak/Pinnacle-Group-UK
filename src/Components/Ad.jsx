758a39d70225a90c516e1d36f3a4d31d3daa2758
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-09 11:20:42 +0530

    Added ad video

--- src/Components/Ad.jsx
+++ src/Components/Ad.jsx
@@ -3,7 +3,7 @@ import Video from "../assets/AdVideo.mp4";
 
 const Ad = () => {
   return (
-    <div className="!px-5 !mb-16">
+    <div className="!p-5 !mb-4 bg-gray-200 border-4 rounded-xl">
       <video
         src={Video}
         className="relative w-full h-auto max-h-[40em] object-cover rounded-xl"
